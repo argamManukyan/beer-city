b2e189f7153f477ed78f8d9ce1d24697594c6ee8
argamManukyan <62460275+argamManukyan@users.noreply.github.com>
2023-03-01 01:16:20 +0400

    product upload

--- shop/views.py
+++ shop/views.py
@@ -109,15 +109,14 @@ class HomePageView(View):
         bullets = Bullets.objects.all()
         video_and_text = Video.objects.filter(location='home').first()
 
-        # new_products = Product.objects.filter(~Q(id__in=used_ids))
-        new_products = Product.objects.all()[:12]
-        # if new_products.exists():
-        #     if len(new_products) < 16:
-        #         settings.SLIDING_COUNT = len(new_products)
-        #     new_products = random.sample(new_products.order_by('?'), settings.SLIDING_COUNT)
-        #     used_ids.extend([i.id for i in new_products])
+        new_products = Product.objects.filter(~Q(id__in=used_ids))
+        if new_products.exists():
+            if len(new_products) < 16:
+                settings.SLIDING_COUNT = len(new_products)
+            new_products = random.sample(new_products.order_by('?')[:50], 12)
+            used_ids.extend([i.id for i in new_products])
 
-        # sale_products = Product.objects.filter(sale__gt=0).exclude(id__in=used_ids).order_by('?')[:settings.SLIDING_COUNT]
+        sale_products = Product.objects.filter(sale__gt=0).exclude(id__in=used_ids).order_by('?')[:16]
         sale_products = Product.objects.filter(sale__gt=0)
 
         context = {
